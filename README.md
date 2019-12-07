0be82e5256539fae9f27470e00ce7402f9f921be
Justin Madru <justin.jdm64@gmail.com>
2019-12-06 23:31:23 -0800

    add setup script to download and install libs

---
+++ README.md
@@ -0,0 +1,4 @@
+
+install libs using:
+
+`bash <(wget -qO- https://raw.githubusercontent.com/jdm64/saphyr-libs/master/setup.sh)`
---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+BRANCH=master
+
+wget https://github.com/jdm64/saphyr-libs/archive/$BRANCH.zip -O saphyr-libs-$BRANCH.zip
+unzip saphyr-libs-$BRANCH.zip
+cd saphyr-libs-$BRANCH
+./install.sh
