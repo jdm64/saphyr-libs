ed2f17c76c61ed978311c99c15dcbc3f67752170
Justin Madru <jmadru@parasoft.com>
2020-01-24 17:15:39 -0800

    always overwrite files when calling unzip

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 BRANCH=master
 
 wget https://github.com/jdm64/saphyr-libs/archive/$BRANCH.zip -O saphyr-libs-$BRANCH.zip
-unzip saphyr-libs-$BRANCH.zip
+unzip -o saphyr-libs-$BRANCH.zip
 cd saphyr-libs-$BRANCH
 ./install.sh
